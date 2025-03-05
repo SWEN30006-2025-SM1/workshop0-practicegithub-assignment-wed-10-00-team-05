93bfc4f027e327f2e6d5bf4f4f82da4ed3441eb0
Joel Basile <jbasile@student.unimelb.edu.au>
2025-03-05 11:39:05 +1100

    Revert "dummy commit"
    This reverts commit 7bb32d04f8549f244691b9c1c16be275e47d243c.

--- app/src/main/java/example/Calculator.java
+++ app/src/main/java/example/Calculator.java
@@ -3,7 +3,7 @@ package example;
 public class Calculator {
     public int add(int a, int b) {
         // TODO: Implement this method
-        return 5;
+        return 4;
     }
 
     public int minus(int a, int b) {
