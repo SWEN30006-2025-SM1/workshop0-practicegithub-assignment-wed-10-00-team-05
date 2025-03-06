bdefca9470232c58fba98fd66c124150a7d907a5
Alice Kjar <akjar@student.unimelb.edu.au>
2025-03-06 11:28:39 +1100

    Added functionality to calculator

--- app/src/main/java/example/Calculator.java
+++ app/src/main/java/example/Calculator.java
@@ -2,25 +2,23 @@ package example;
 
 public class Calculator {
     public int add(int a, int b) {
-        // TODO: Implement this method
 
-        //dummy commit 2
-        return 5;
+        return a + b;
     }
 
     public int minus(int a, int b) {
-        // TODO: Implement this method
-        //testing
-        return 10;
+        return a - b;
     }
 
     public int multiply(int a, int b) {
-        // TODO: Implement this method
-        return 10;
+        return a * b;
     }
 
     public double divide(int a, int b) {
-        // TODO: Implement this method
-        return 5;
+        if (b == 0) {
+            System.out.println("Divide by zero error");
+            System.exit(-1);
+        }
+        return (double)a/b;
     }
 }
